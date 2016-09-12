d44ea7cc961419a796f0440ce29a18a9a509e94b
Jun Mark Torii <j.torii4@gmail.com>
2016-09-12 00:36:58 -0400

    Updated images

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -17,7 +17,7 @@ $(document).ready(function() {
 	
 	$('#home').height($(window).height()+50);
 	
-	$.backstretch('assets/images/header-bg.jpg');
+	$.backstretch('assets/images/header-bg.png');
 	
 	if($(window).scrollTop() > ($(window).height()+50)){
 		$('.backstretch').hide();
